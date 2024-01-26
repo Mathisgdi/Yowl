73061d4039ccb2257e35458276f4aa7c0cc3e237
AntoineL2028 <146076967+AntoineL2028@users.noreply.github.com>
2024-01-26 11:42:13 +0100

    [FIX] merge

--- App.js
+++ App.js
@@ -1,23 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
+<<<<<<< HEAD
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity } from 'react-native';
+import Login from '.app/screens/Login';
 import tw from 'twrnc';
+=======
+import { Text, View } from 'react-native';
+>>>>>>> 059e4489e08067886fd3982a1ae47547dc0338f1
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={tw`flex-1 justify-center items-center bg-red`}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Martin </Text>
-
+<<<<<<< HEAD
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+=======
+    <View classname = "flex-1 justify-center items-center bg-red">
+    <Text>Open up App.js to start working on your app!</Text>
+    <Text>Martin </Text>
+    <Text style={`tw text-white text-3xl font-bold|f-Hello World`}> </Text>
       <StatusBar style="auto" />
     </View>
+>>>>>>> 059e4489e08067886fd3982a1ae47547dc0338f1
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ FirebaseConfig.ts
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBjb77qs1gQ5jaiX8w0KJWlcqKAAbSgLjo",
+  authDomain: "yowl-join.firebaseapp.com",
+  projectId: "yowl-join",
+  storageBucket: "yowl-join.appspot.com",
+  messagingSenderId: "581292534351",
+  appId: "1:581292534351:web:bff3659df3eb4080f9583a",
+  measurementId: "G-KM0RNGP8C7"
+};
+
+// Initialize Firebase
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export const FIREBASE_DB = getFirestore(FIREBASE_APP);
