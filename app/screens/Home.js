05867f5cc01835853f63e4b8691d117904dd7bf3
Mathisgdi <mathis.ghidotti@epitech.digital>
2024-02-02 11:37:05 +0100

    [FEAT] Realtime update POST

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,27 +1,23 @@
 import { Text, View } from "react-native";
 import React, { useState, useEffect } from "react";
 import { FIREBASE_DB } from "../../FirebaseConfig";
-import { collection, setDoc, getDocs } from "firebase/firestore";
+import { collection, setDoc, getDocs, onSnapshot, query } from "firebase/firestore";
 
 const ShowData = () => {
   const db = FIREBASE_DB;
   const [postData, setPostData] = useState([]);
-  const fetchData = async () => {
-    try {
-      const querySnapshot = await getDocs(collection(db, "posts"));
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(db, "posts"), (querySnapshot) => {
       const postsData = [];
       querySnapshot.forEach((doc) => {
         postsData.push({ id: doc.id, ...doc.data() });
       });
       setPostData(postsData);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []); // Execute la fonction que une fois
+    });
+    return () => {
+      unsubscribe(); // Lorsque le composant est démonté, cela arrête la surveillance des modifications
+    };
+  }, [db]);
 
   return (
     <View>
